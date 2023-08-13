ed1b52b72ef4aa824ca11106798e87d7e85ea27c
Sawarz <sawarz22@gmail.com>
2023-08-13 10:43:00 +0200

    Fix build

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
@@ -15,7 +13,7 @@ function App() {
 		setFruit(e.target.value);
 	}
 
-	function handleOrder(e: any) {
+	function handleOrder() {
 		alert("Teraz sobie możesz taki upiec :P");
 	}
 
